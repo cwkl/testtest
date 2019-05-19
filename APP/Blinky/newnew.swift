06b05600c6049e3e383821e24ec41dfe432e54eb
KWON JUNHYEOK <cwkl2540@gmail.com>
2019-05-19 17:33:01 +0900

    no message

---
+++ APP/Blinky/newnew.swift
@@ -0,0 +1,13 @@
+//
+//  newnew.swift
+//  Blinky
+//
+//  Created by Junhyeok Kwon on 2019. 5. 19..
+//  Copyright © 2019년 Junhyeok Kwon. All rights reserved.
+//
+
+import Foundation
+
+class kwongod{
+    let kwon: String = "kwongod"
+}
